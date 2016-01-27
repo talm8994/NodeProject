f56c04a6929beb1b855a26d5a03572049cc6893e
talm8994 <tom.tom.pop13@gmail.com>
2016-01-27 13:03:05 -0700

    node

---
+++ src/Controller/NodeController.h
@@ -0,0 +1,17 @@
+/*
+ * NodeController.h
+ *
+ *  Created on: Jan 27, 2016
+ *      Author: talm8994
+ */
+
+#ifndef CONTROLLER_NODECONTROLLER_H_
+#define CONTROLLER_NODECONTROLLER_H_
+
+class NodeController {
+public:
+	NodeController();
+	virtual ~NodeController();
+};
+
+#endif /* CONTROLLER_NODECONTROLLER_H_ */
---
+++ src/Controller/NodeRunner.cpp
@@ -0,0 +1,17 @@
+/*
+ * NodeRunner.cpp
+ *
+ *  Created on: Jan 27, 2016
+ *      Author: talm8994
+ */
+#include "NodeController.h"
+
+int main()
+{
+	NodeController * baseController = new NodeController();
+	baseController->start();
+	return 0;
+}
+
+
+
