9fee9629ff5d52249212ba5d8acafe8c7ba73305
talm8994 <tom.tom.pop13@gmail.com>
2016-02-02 12:37:54 -0700

    new arrays

---
+++ src/Model/CTECArray.cpp
@@ -0,0 +1,18 @@
+/*
+ * CTECArray.cpp
+ *
+ *  Created on: Feb 2, 2016
+ *      Author: talm8994
+ */
+
+#include "CTECArray.h"
+
+CTECArray::CTECArray() {
+	// TODO Auto-generated constructor stub
+
+}
+
+CTECArray::~CTECArray() {
+	// TODO Auto-generated destructor stub
+}
+
